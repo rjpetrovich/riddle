079150cd0a1a087efdcf7027e6b7f0d0237a7683
rjpetrovich <76408212+rjpetrovich@users.noreply.github.com>
2022-01-28 21:54:32 -0400

    Add files via upload

---
+++ assets/js/incio.js
@@ -0,0 +1,14 @@
+function ir (){
+
+    var c = "señorita"
+    var u = "Maria"
+
+    if(document.forms.password.value==c && document.forms.login.value==u){
+        alert("SEHR GUT!!!");
+        windows.location="video.html";}
+
+    else{
+        alert("NEIN.. NEIN.. NEIN.. Versuch ist noch einmal. ")
+    }
+
+}
---
+++ inicio.js
@@ -0,0 +1,14 @@
+function ir (){
+
+    var c = "la nariz";
+    var u = "el corazon";
+
+    if(document.form.password.value==c && document.form.login.value==u){
+        alert("SEHR GUT!!!");
+    window.location="video.html";}
+
+    else{
+        alert("NEIN.. NEIN.. NEIN.. Versuch ist noch einmal. ")
+    }
+
+}
